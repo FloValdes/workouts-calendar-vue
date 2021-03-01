cbf86e17c0e1e9ff4fb0a6a119d70007da27c771
FloValdes <ftvaldes1@uc.cl>
2021-03-01 17:24:28 -0300

    add create event from day and 2nd and 3rd graph

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,146 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <div class='parent-main'>
+        <calendar class='calendar-main' :events="events" @eventsChanged="events = $event; getChartData()"></calendar>
+        <div class='graph-main'>
+          <div class='text-h3'>Totales</div>
+          <bar-chart :chart-data="chartDataDifficulty" :options="options"></bar-chart>
+          <bar-chart :chart-data="chartDataBodyPart" :options="options"></bar-chart>
+          <bar-chart :chart-data="chartDataDuration" :options="options"></bar-chart>
+        </div>
+      </div>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calendar from './components/Calendar';
+import BarChart from './components/BarChart';
+import { db } from '@/main';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    'calendar': Calendar,
+    'bar-chart': BarChart
+  },
+
+  data: () => ({
+    events: [],
+    difficulties: ['1', '2', '3', '4', '5'],
+    bodyParts: ['Core', 'Lower Body', 'Total Body', 'Upper Body'],
+    durations: ['<15', '15-30', '30-45', '45-60', '60<'],
+    chartDataDifficulty: {},
+    chartDataBodyPart: {},
+    chartDataDuration: {},
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    stepSize: 1
+                }
+            }]
+        }
+    },
+  }),
+
+  mounted () {
+    this.loadData();
+  },
+
+  methods: {
+    async loadData() {
+      await this.getEvents();
+      await this.getChartData();
+    },
+
+    async getEvents() {
+      let snapshot = await db.collection('calEvent').get();
+      let events = [];
+      snapshot.forEach(doc => {
+        let appData = doc.data();
+        appData.id = doc.id;
+        events.push(appData);
+      });
+      this.events = events;
+    },
+
+    async getChartData() {
+      const diffCount = [];
+      this.difficulties.forEach((diff) => {
+        diffCount.push(this.events.filter(c => c.difficulty[0] === diff).length);
+      })
+      const newDataDifficulty = {
+        labels: this.difficulties,
+        datasets: [
+          {
+            label: 'Difficulty',
+            backgroundColor: '#f87979',
+            data: diffCount
+          }
+        ]
+      } 
+
+      const bodyCount = [];
+      this.bodyParts.forEach((bodyPart) => {
+        bodyCount.push(this.events.filter(c => c.bodyFocus.split(',').map((e) => e.trim()).indexOf(bodyPart) !== -1).length);
+      })
+      const newDataBodyPart = {
+        labels: this.bodyParts,
+        datasets: [
+          {
+            label: 'Body Part',
+            backgroundColor: '#00B97C',
+            data: bodyCount
+          }
+        ]
+      }
+
+      const binRange = {'<15': [-1, 15], '15-30': [15, 30], '30-45': [30, 45], '45-60': [45, 60], '60<': [60, Infinity]}
+      const durationCount = [];
+      this.durations.forEach((duration) => {
+        durationCount.push(this.events.filter(c => Number(c.duration.split(' ')[0]) >= binRange[duration][0] && Number(c.duration.split(' ')[0]) < binRange[duration][1]).length)
+      })
+      const newDataDuration = {
+        labels: this.durations,
+        datasets: [
+          {
+            label: 'Duration',
+            backgroundColor: '#A090FF',
+            data: durationCount
+          }
+        ]
+      }
+
+      this.chartDataDifficulty = newDataDifficulty;
+      this.chartDataBodyPart = newDataBodyPart;
+      this.chartDataDuration = newDataDuration;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .parent-main {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  .calendar-main {
+    flex: 1 1 1000px;
+    margin: 5px;
+  }
+
+  .graph-main {
+    flex: 1 1 200px;
+    margin: 5px;
+    height: 95vh;
+    text-align: center;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+// import axios from 'axios';
+// import cheerio from 'cheerio';
+
+Vue.use(VueTextareaAutosize);
 
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+  apiKey: "AIzaSyDpQN5Frt8yb7yl9C-pMRY_Gk_3qB90rwk",
+  authDomain: "vue-calendar-1785f.firebaseapp.com",
+  projectId: "vue-calendar-1785f",
+  storageBucket: "vue-calendar-1785f.appspot.com",
+  messagingSenderId: "1012487929714",
+  appId: "1:1012487929714:web:4c10fadbdd226b1a8be7eb"
+});
+
+export const db = firebase.firestore();
+// export axios;
+// export cheerio;
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
