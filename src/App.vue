dc7f572fb61fa4ca46fc0fc3b0861bcb7f23ea12
FloValdes <ftvaldes1@uc.cl>
2021-03-13 16:12:48 -0300

    add workouts list

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,44 @@
           <bar-chart :chart-data="chartDataDuration" :options="options"></bar-chart>
         </div>
       </div>
+      <v-container>
+        <div class='text-h2 text-center'>Workouts</div>
+        <v-text-field
+          v-model="searchWorkoutTable"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+        <v-data-table
+          :headers="workoutTableHeaders"
+          :items="events"
+          :items-per-page="15"
+          class="elevation-1"
+          :search="searchWorkoutTable"
+        >
+          <!-- <template
+            v-slot:body="{ items }"
+          >
+            <tbody>
+              <tr
+                v-for="item in items"
+                :key="item.name"
+              >
+                <td v-for="header in workoutTableHeaders" :key="header.value">{{header.value === 'favorite' ? item[header.value] ? "Yes" : 'No' : item[header.value]}}</td>
+              </tr>
+            </tbody>
+          </template> -->
+          <template v-slot:[`item.favorite`]="{ item }">
+            <v-chip
+              :color="item.favorite ? 'green' : 'red'"
+              dark
+            >
+              {{ item.favorite ? 'Yes' : 'No' }}
+            </v-chip>
+          </template>
+        </v-data-table>      
+      </v-container>
     </v-main>
   </v-app>
 </template>
@@ -47,6 +85,20 @@ export default {
             }]
         }
     },
+    workoutTableHeaders: [
+      {
+        text: 'Workout',
+        align: 'start',
+        sortable: false,
+        value: 'name',
+      },
+      { text: 'Body Focus', value: 'bodyFocus' },
+      { text: 'Duration', value: 'duration' },
+      { text: 'Favorite', value: 'favorite' },
+      // { text: 'Protein (g)', value: 'protein' },
+      // { text: 'Iron (%)', value: 'iron' },
+    ],
+    searchWorkoutTable: ""
   }),
 
   mounted () {
@@ -67,6 +119,7 @@ export default {
         appData.id = doc.id;
         events.push(appData);
       });
+      console.log(events);
       this.events = events;
     },
 
